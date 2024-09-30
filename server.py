26af8b23c1237181b1159feb6a82237d5c2a686f
NekoMamushi7812 <gatomundogaturro78@gmail.com>
2024-09-30 05:40:04 +0000

    Mundo guardado 2024-09-30

--- server.py
+++ server.py
@@ -8,18 +8,18 @@
 # Regiones de ngrok
 # Código          Lugar
 #-----------      ---------------------------
-# ap	          Asia / Pacífico (Singapore)
+# ap	          Asia / Pacífico (Singapore) 
 # au		      Australia (Sydney)
-# eu		      Europa (Frankfurt)
-# in		      India (Mumbai)
-# jp		      Japón (Tokyo)
+# eu		      Europa (Frankfurt) 
+# in		      India (Mumbai)  
+# jp		      Japón (Tokyo) 
 # sa		      Sudamérica (São Paulo)
-# us		      Estados unidos (Ohio)
-# us-cal-1	      Estados unidos (California)
+# us		      Estados unidos (Ohio) 
+# us-cal-1	      Estados unidos (California) 
 
   
 
- 
+  
 
  
 
