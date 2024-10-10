15862b6753aa46f61be8f3c7033aad674b76979c
NekoMamushi7812 <gatomundogaturro78@gmail.com>
2024-10-10 01:49:23 +0000

    Mundo guardado 2024-10-10

--- server.py
+++ server.py
@@ -11,9 +11,9 @@
 # ap	          Asia / Pacífico (Singapore)
 # au		      Australia (Sydney) 
 # eu		      Europa (Frankfurt) 
-# jp		      Japón (Tokyo) 
+# jp		      Japón (Tokyo)  
 # sa		      Sudamérica (São Paulo) 
-# us		      Estados unidos (Ohio)  
+# us		      Estados unidos (Ohio)   
 # us-cal-1	      Estados unidos (California)  
  
    
